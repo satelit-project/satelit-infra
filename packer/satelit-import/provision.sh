1f300ee3fa56a2da3ced6f04531b1c1230622c71
Igor Nikitin <rabbitinspace@icloud.com>
2020-03-15 23:05:39 +1100

    hard work

---
+++ packer/satelit-import/provision.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Prepares Ubuntu image to run anime import services.
+
+set -euo pipefail
+
+prepare_apt() {
+  sudo apt-get update
+  sudo apt-get install \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg-agent \
+      software-properties-common
+}
+
+install_docker() {
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) \
+      stable"
+
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
+}
+
+main() {
+    prepare_apt
+    install_docker
+}
+
+main "$@"
