e1de4ce62a36c1b0b5a76bdaa6ed1e8f9ed9f948
Igor Nikitin <rabbitinspace@icloud.com>
2020-03-16 20:34:13 +1100

    fix vagrant box

--- packer/satelit-import/provision.sh
+++ packer/satelit-import/provision.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 prepare_apt() {
   sudo apt-get update
-  sudo apt-get install \
+  sudo apt-get install -y \
       apt-transport-https \
       ca-certificates \
       curl \
@@ -22,7 +22,7 @@ install_docker() {
       stable"
 
   sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 }
 
 main() {
